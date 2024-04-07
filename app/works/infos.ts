a8dbe0cbafaeae8f6d4fa2ee5575af76ee7e5624
ZRNOF <ZRNOF@protonmail.com>
2024-04-07 21:32:47 +0800

    build works page

---
+++ app/works/infos.ts
@@ -0,0 +1,39 @@
+const infos = [
+	{
+		id: "UnifiedRupture",
+		title: "Unified Rupture",
+		desc: "description",
+		href: "https://openprocessing.org/sketch/1898796",
+		preview: "/UnifiedRupture.png",
+	},
+	{
+		id: "Travel",
+		title: "Travel",
+		desc: "description",
+		href: "https://openprocessing.org/sketch/1826376",
+		preview: "/Travel.png",
+	},
+	{
+		id: "DraculaAndTheUndead",
+		title: "Dracula and the Undead",
+		desc: "description",
+		href: "https://openprocessing.org/sketch/1891135",
+		preview: "/DraculaAndTheUndead.png",
+	},
+	{
+		id: "SHUTTLE",
+		title: "SHUTTLE !",
+		desc: "description",
+		href: "https://openprocessing.org/sketch/1828512",
+		preview: "/SHUTTLE.png",
+	},
+	{
+		id: "Naraka",
+		title: "Naraka",
+		desc: "description",
+		href: "https://openprocessing.org/sketch/1914052",
+		preview: "/Naraka.png",
+	},
+]
+
+export default infos
--- app/works/page.tsx
+++ app/works/page.tsx
@@ -1,8 +1,97 @@
+"use client"
+
+import Image from "next/image"
+import "./test.css"
+import { Flex, Box, Heading, Grid, Text } from "@radix-ui/themes"
+import Link from "next/link"
+import infos from "./infos"
+
 const WorksPage = () => {
 	return (
-		<>
-			<div>Works</div>
-		</>
+		<Flex
+			position="fixed"
+			top="0"
+			left="0"
+			width="100dvw"
+			height="100dvh"
+			justify="center"
+			overflowY="scroll"
+		>
+			<Box width="100%" height="100%" maxWidth="1136px">
+				{infos.map((info) => {
+					return (
+						<Grid
+							key={info.id}
+							columns="repeat(10, 1fr)"
+							rows="repeat(1, 1fr)"
+							align="center"
+							width="100%"
+							height="100%"
+							py="7rem"
+							px="4rem"
+						>
+							<Box
+								gridColumnStart="1"
+								gridColumnEnd={{ initial: "11", sm: "6" }}
+								gridRow="1"
+								p="2"
+								pr={{ initial: "0", sm: "7" }}
+								className="z-10 bg-[#000000E7] md:bg-transparent"
+							>
+								<Heading as="h2" align="center">
+									{info.title}
+								</Heading>
+								<hr className="my-2 hidden md:block" />
+								<Text className="hidden md:inline">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Adipisci ea necessitatibus quod eaque, laudantium, odit
+									consequuntur illo quia corporis reprehenderit saepe explicabo
+									assumenda libero eveniet commodi, natus perspiciatis facere
+									maiores?
+								</Text>
+							</Box>
+							<Box
+								gridColumnStart={{ initial: "1", sm: "6" }}
+								gridColumnEnd="11"
+								gridRow="1"
+								id={info.id}
+								width="100%"
+								maxHeight={{ initial: "", xs: "80%" }}
+								className="relative aspect-square overflow-hidden outline outline-8 outline-[#ccc]"
+							>
+								<Preview src={info.preview} />
+								<ViewCodeButton href={info.href} />
+							</Box>
+						</Grid>
+					)
+				})}
+			</Box>
+		</Flex>
+	)
+}
+
+const Preview = ({ src }: { src: string }) => {
+	return (
+		<Image
+			src={src}
+			loading="lazy"
+			alt=""
+			width={1024}
+			height={1024}
+			className="absolute w-full h-full object-cover transition-opacity duration-500 hover:opacity-0"
+		/>
+	)
+}
+
+const ViewCodeButton = ({ href }: { href: string }) => {
+	return (
+		<Link
+			className="text-xs px-2 py-1 bg-black text-white source-button bottom-0 right-0 absolute"
+			href={href}
+			target="_blank"
+		>
+			View code
+		</Link>
 	)
 }
 
