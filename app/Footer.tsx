eb9ba605f5322dca64ab8cc7d6a6ac2ea109cb0a
ZRNOF <ZRNOF@protonmail.com>
2024-04-23 11:34:34 +0800

    update footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -8,18 +8,9 @@ const Footer = () => {
 			align="end"
 		>
 			<Container>
-				<Flex
-					direction={{ initial: "column", xs: "row" }}
-					justify="center"
-					align="center"
-					className="h-16"
-					gap="1"
-				>
+				<Flex justify="center" align="center" className="h-16" gap="1">
 					<Text className="text-[var(--fg-color)] text-[10px]">
 						Copyright © 2024 Zaron Chen.
-					</Text>
-					<Text className="text-[var(--fg-color)] text-[10px]">
-						All rights reserved.
 					</Text>
 				</Flex>
 			</Container>
